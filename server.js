9ebd8e1c9e65c9015193cbd0a21a0f0bc9a72352
FORTI2111 <eryyk016@gmail.com>
2026-04-30 16:38:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const axios = require("axios");
 const app = express();
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
+console.log("KEY:", process.env.STRIPE_SECRET_KEY);
+
 app.use(cors());
 app.use(express.json());
 
